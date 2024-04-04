f56812513e7ce708cd0651f352140cfa64d068ee
Irina Vorobeva <i.s.vorobjeva@yandex.ru>
2024-04-04 17:57:37 +0500

    step 11 complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,6 +12,12 @@ import { actions as usersActions } from "./slices/userSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
 import { ToastContainer } from "react-toastify";
+import { Provider, ErrorBoundary } from "@rollbar/react";
+
+const rollbarConfig = {
+    accessToken: "a070cd6a84bd42bbb1a352271473b304",
+    environment: "testenv",
+};
 
 function App() {
     const dispatch = useDispatch();
@@ -41,29 +47,33 @@ function App() {
     };
 
     return (
-        <div className="d-flex flex-column h-100">
-            <Navbar className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
-                <div className="container justify-content-between">
-                    <Navbar.Brand href="/">Hexlet Chat</Navbar.Brand>
-                    {isAuth && (
-                        <Button type="button" onClick={userLogout}>
-                            {t("buttons.logout")}
-                        </Button>
-                    )}
+        <Provider config={rollbarConfig}>
+            <ErrorBoundary>
+                <div className="d-flex flex-column h-100">
+                    <Navbar className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+                        <div className="container justify-content-between">
+                            <Navbar.Brand href="/">Hexlet Chat</Navbar.Brand>
+                            {isAuth && (
+                                <Button type="button" onClick={userLogout}>
+                                    {t("buttons.logout")}
+                                </Button>
+                            )}
+                        </div>
+                    </Navbar>
+                    <div className="d-flex flex-column h-100 bg-body-tertiary">
+                        <BrowserRouter>
+                            <Routes>
+                                <Route path="/" element={<MainPage />} />
+                                <Route path="login" element={<LoginPage />} />
+                                <Route path="signup" element={<SignupPage />} />
+                                <Route path="*" element={<NotFoundPage />} />
+                            </Routes>
+                        </BrowserRouter>
+                    </div>
+                    <ToastContainer />
                 </div>
-            </Navbar>
-            <div className="d-flex flex-column h-100 bg-body-tertiary">
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<MainPage />} />
-                        <Route path="login" element={<LoginPage />} />
-                        <Route path="signup" element={<SignupPage />} />
-                        <Route path="*" element={<NotFoundPage />} />
-                    </Routes>
-                </BrowserRouter>
-            </div>
-            <ToastContainer />
-        </div>
+            </ErrorBoundary>
+        </Provider>
     );
 }
 
